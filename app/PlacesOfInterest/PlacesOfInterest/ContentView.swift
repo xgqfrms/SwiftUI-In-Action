24609540e6cf3d68fbbf5924ac4a96cae48a867e
xgqfrms <xgqfrms@outlook.com>
2022-05-19 16:29:48 +0800

    feat: add JSON

---
+++ app/PlacesOfInterest/PlacesOfInterest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlacesOfInterest
+//
+//  Created by 夏凌晨 on 2022/5/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app/PlacesOfInterest/PlacesOfInterest/PlacesOfInterestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesOfInterestApp.swift
+//  PlacesOfInterest
+//
+//  Created by 夏凌晨 on 2022/5/19.
+//
+
+import SwiftUI
+
+@main
+struct PlacesOfInterestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
