de4757f596ee126dc50b9702b28fa2b06a5fe041
xgqfrms <xgqfrms@outlook.com>
2022-05-23 22:52:27 +0800

    animation

--- app/TodoList/TodoList/Views/NoItemsView.swift
+++ app/TodoList/TodoList/Views/NoItemsView.swift
@@ -8,13 +8,73 @@
 import SwiftUI
 
 struct NoItemsView: View {
+  @State private var animate: Bool = false;
+  func autoAnimate() {
+    // delay 1.5s
+    DispatchQueue.main.asyncAfter(
+      deadline: .now() + 1.5,
+      execute: {
+        guard !animate else {
+          return;
+        }
+        /*
+        withAnimation(
+          Animation
+            .easeInOut(duration: 2)
+            .repeatForever(),
+          {
+            animate.toggle();
+          }
+        )
+        */
+        withAnimation(
+          Animation
+            .easeInOut(duration: 2)
+            .repeatForever()
+        ) {
+          animate.toggle();
+        }
+      }
+    )
+  }
   var body: some View {
-     Text("NoItem")
+    ScrollView {
+      VStack(spacing: 10) {
+        Text("NoItem")
+          .font(.largeTitle)
+        Text("NoItemDesc")
+        NavigationLink(
+          destination: AddItemView(),
+          label: {
+            Text("GoToAdd")
+              .font(.headline)
+              .frame(height: 55)
+              .frame(maxWidth: .infinity)
+              .foregroundColor(.white)
+              .background(animate ? Color.blue : Color.pink)
+              // .background(Color.accentColor)
+              .cornerRadius(10)
+              .padding(.horizontal, animate ? 30 : 50)
+              .scaleEffect(animate ? 1.1 : 1.0)
+              .offset(y: animate ? 10 : 0)
+              .shadow(
+                color: animate ? .blue.opacity(0.7) : .pink.opacity(0.3),
+                radius: animate ? 20 : 10,
+                x: 0,
+                y: animate ? 20 : 10)
+          }
+        )
+      }
+      .multilineTextAlignment(.center)
+      .padding(40)
+      .onAppear(perform: autoAnimate)
+    }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
   }
 }
 
-//struct NoItemsView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        NoItemsView()
-//    }
-//}
+struct NoItemsView_Previews: PreviewProvider {
+  static var previews: some View {
+    NoItemsView()
+  }
+}
