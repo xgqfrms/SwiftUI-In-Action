a5432a7a4bf9a994b412e563e72b4e9d21054db9
xgqfrms <xgqfrms@outlook.com>
2022-06-02 12:40:40 +0800

    feat: 扩展原有的 Bundle ✅

--- app/PlacesOfInterest/PlacesOfInterest/Extensions/CodableBundle.swift
+++ app/PlacesOfInterest/PlacesOfInterest/Extensions/CodableBundle.swift
@@ -7,12 +7,10 @@
 
 import Foundation
 
-// 扩展原有的 Bundle，还是自定义新的 extension ???
+// 扩展原有的 Bundle ✅
 extension Bundle {
-  // Extensions must not contain stored properties
-  // private let step1Error = "加载本地 json 文件 ❌";
-  // 解析 json 文件
-  func decode(_ fileName: String) -> [CoverModel] {
+  // 解析 json 文件, T 泛型
+  func decode<T: Decodable>(_ fileName: String) -> T {
     // 1. 加载本地 json 文件
     guard let url = self.url(forResource: fileName, withExtension: nil) else {
       let error = "加载本地 json 文件 ❌, fileName: \(fileName)";
@@ -26,7 +24,8 @@ extension Bundle {
       fatalError(error)
     }
     // 3. 解析 JSON 数据
-    guard let json = try? JSONDecoder().decode([CoverModel].self, from: data) else {
+    let decoder = JSONDecoder();
+    guard let json = try? decoder.decode(T.self, from: data) else {
       let error = "解析 JSON 数据 ❌, fileName: \(fileName)";
       print(error)
       fatalError(error)
@@ -34,3 +33,35 @@ extension Bundle {
     return json;
   }
 }
+
+/*
+// 扩展原有的 Bundle ✅
+extension Bundle {
+  // Extensions must not contain stored properties
+  // private let step1Error = "加载本地 json 文件 ❌";
+  // 解析 json 文件
+  func decodeDemo(_ fileName: String) -> [CoverModel] {
+    // 1. 加载本地 json 文件
+    guard let url = self.url(forResource: fileName, withExtension: nil) else {
+      let error = "加载本地 json 文件 ❌, fileName: \(fileName)";
+      print(error)
+      fatalError(error)
+    }
+    // 2. 读取数据
+    guard let data = try? Data(contentsOf: url) else {
+      let error = "读取数据 ❌, fileName: \(fileName)";
+      print(error)
+      fatalError(error)
+    }
+    // 3. 解析 JSON 数据
+    let decoder = JSONDecoder();
+    guard let json = try? decoder.decode([CoverModel].self, from: data) else {
+      let error = "解析 JSON 数据 ❌, fileName: \(fileName)";
+      print(error)
+      fatalError(error)
+    }
+    return json;
+  }
+}
+
+ */
